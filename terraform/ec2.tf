a2a419c727f45135cfaa3713d4bfecba132628ee
Rukaya Ogazi-Khan <34509633+RukayaOK@users.noreply.github.com>
2022-12-09 08:30:59 +0000

    Remove tag from ec2 instance

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,8 +2,4 @@ resource "aws_instance" "instance" {
   ami           = var.instance_ami
   instance_type = var.instance_type
   key_name      = aws_key_pair.key_pair.key_name
-
-  tags = {
-    Name = "${var.instance_type}"
-  }
 }
